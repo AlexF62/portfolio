69a0d1d3493ce63a4e70d10600aadbb5f8aa29b1
Aleksandr Fomichev <fomichevae28@gmail.com>
2024-10-11 18:43:27 +0200

    finish-section-slider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './layout/header/Header';
 import AboutMe from './layout/sections/aboutme/AboutMe';
 import Main from './layout/sections/main/Main';
+import { Slider } from './layout/sections/slider/Slider';
+
 import { BackgroundWrapper } from './styles/theme/WrapperBackground';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
                 <Main />
             </BackgroundWrapper>
             <AboutMe />
+            <Slider />
         </>
     );
 }
---
+++ src/layout/sections/slider/Slider.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import styled from 'styled-components';
+import photo from '../../../assets/icons/Rectangle.svg';
+
+const SlideContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+`;
+
+const IconWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 60px;
+    height: 60px;
+
+    border-radius: 50%;
+    margin-bottom: 20px;
+`;
+
+const Icon = styled.img`
+    width: 30px;
+    height: 30px;
+    color: #f39c12;
+`;
+
+const SlideTitle = styled.h3`
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin-bottom: 10px;
+    color: #333;
+`;
+
+const SlideText = styled.p`
+    font-size: 1rem;
+    color: #777;
+    max-width: 250px;
+`;
+
+const SlideContent: React.FC = () => {
+    return (
+        <>
+            <SlideContainer>
+                <IconWrapper>
+                    <Icon src={photo} alt='Branding Icon' />
+                </IconWrapper>
+                <SlideTitle>Branding Design</SlideTitle>
+                <SlideText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Orci eget mi elit cursus donec amet sed sagittis.
+                </SlideText>
+            </SlideContainer>
+        </>
+    );
+};
+
+const StyledSlider = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 150px;
+`;
+
+const StyledSliderContainer = styled.div`
+    max-width: 1270px;
+    height: 50vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+`;
+
+const TitleContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h3 {
+        font-size: 60px;
+        color: ${(props) => props.theme.colors.titleColor};
+    }
+    p {
+        color: #868686;
+        font-size: 18px;
+    }
+`;
+
+const responsive = {
+    0: { items: 1 },
+    568: { items: 2 },
+    1024: { items: 3 },
+};
+
+const StyledDots = styled.div`
+    .alice-carousel__dots-item {
+        background-color: #ddd;
+    }
+
+    .alice-carousel__dots-item.__active {
+        background-color: ${(props) => props.theme.colors.secondary};
+    }
+`;
+
+const items = [
+    <SlideContent key='1' />,
+    <SlideContent key='1' />,
+    <SlideContent key='1' />,
+    <SlideContent key='2' />,
+    <SlideContent key='2' />,
+    <SlideContent key='2' />,
+];
+
+export const Slider = () => (
+    <StyledSlider>
+        <TitleContent>
+            <h3>What I Offer</h3>
+            <p>Things that I can do for my clients. Just make your good</p>
+            <p>trust I love to provide quality works.</p>
+        </TitleContent>
+        <StyledDots>
+            <StyledSliderContainer>
+                <AliceCarousel
+                    items={items}
+                    responsive={responsive}
+                    disableButtonsControls
+                />
+            </StyledSliderContainer>
+        </StyledDots>
+    </StyledSlider>
+);
+
+export default Slider;
