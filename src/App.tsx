e9e09820f2889530663925a001faa0e4ae727f45
Aleksandr Fomichev <fomichevae28@gmail.com>
2024-10-15 15:18:33 +0200

    finish desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Footer from './layout/footer/Footer';
 import Header from './layout/header/Header';
 import AboutMe from './layout/sections/aboutme/AboutMe';
 import Brands from './layout/sections/brands/Brands';
 import Main from './layout/sections/main/Main';
 import Portfolio from './layout/sections/portfolio/Portfolio';
+import Posts from './layout/sections/posts/Posts';
 import Reviews from './layout/sections/reviews/Reviews';
 import { Slider } from './layout/sections/slider/Slider';
 
@@ -21,6 +23,8 @@ function App() {
             <Portfolio />
             <Brands />
             <Reviews />
+            <Posts />
+            <Footer />
         </>
     );
 }
